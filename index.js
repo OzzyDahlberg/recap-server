7456d2da1e4208ac0533e27b67794e8bed8f1de4
OzzyDahlberg <ozzy.dahlberg@hotmail.com>
2025-01-27 10:45:31 +0100

    edits to portions of code

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // * imports 
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
 
-const mongoose = require("mongoose")
-const express = require("express")
-const cors = require("cors")
-
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = ProcessingInstruction.env.PORT || 5000
-const app = express()
+const PORT = process.env.PORT || 4001
+const app = express();
 
+// ! DB
+
+// ? Cors
+app.use(cors())
+
+// ? body parser
+app.use(express.json)
+
+// ? routes
+app.get("/", (req, res) => {
+res.send("welcome to my api")
+})
+
+// ! app.use("/api/users", userRoutes)
+// ! app.use("/api/auth", authRoutes)
+
+// ? Logs
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`)
+})
