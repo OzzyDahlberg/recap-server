072a1fcce8b586b571a7a244cc59503e512c536e
OzzyDahlberg <ozzy.dahlberg@hotmail.com>
2025-01-27 09:51:09 +0100

    unfinished server files

---
+++ index.js
@@ -0,0 +1,12 @@
+// * imports 
+
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+
+const dotenv = require("dotenv")
+dotenv.config();
+
+const PORT = ProcessingInstruction.env.PORT || 5000
+const app = express()
+
